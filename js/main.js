cccfcbeb1c14e2bc711c7d61486160141ad6342f
rlam1217 <rlam1217@gmail.com>
2019-06-05 13:44:38 -0700

    builds responsive layout for mobile, tablet, desktop

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(function() {
 
     
 
-    $('[name=categories]').on("change", function() {
+    $('[name=categories]').on('change', function() {
         
         const categories = $(this).val();
         console.log(categories);
@@ -18,22 +18,39 @@ $(function() {
              const results = $(data.results)
              console.log(results);
 
-            $(".article-container ul").empty(); //empties articles on category change
-            $(results).each(function(index, element) {
-                
+             $('.article-container ul').empty(); //empties articles on category change
+             $(results).each(function(index, element) {
+              
                  
-                 if (element.multimedia.length >= 5 && $count < 12) {
-                  console.log(element.multimedia[4].url);
-                  console.log(element.short_url);
+                  if (element.multimedia.length >= 5 && $count < 12) {
+                   console.log(element.multimedia[4].url);
+                   console.log(element.short_url);
                   
-                  $(".article-container ul").append('<li>' + '<a href="' + element.short_url + '">' + '<img src="' + element.multimedia[4].url + '">' + '<p>' + element.title + '</p>' + '</a>' + '</li>');
+
+                  let image = '<div class="article-image" style="background-image:url(' + element.multimedia[4].url + ');">' + '</div>';
+                  let url = '<a href="' + element.short_url + '">';
+                  let p = '<p>' + element.abstract + '</p>';
                   
-                  // $('.article-container ul').append('<li>' + element.multimedia[1].caption + '</li>');
+      
+                   console.log(image);
+
+                   $('.article-container ul').append('<li class="article-list" >' + url + image + p + '</a>' + '</li>');
                   
-                    $count++;
-                 }
+                  //  $('.article-container ul').append('<li class="article-list" >' + '<a href="' + element.short_url + '">' + image + '<p>' + element.abstract + '</p>' + '</a>' + '</li>');
+
+                   // $(".article-container ul").append('<li>' + '<a href="' + element.short_url + '">' + img + '</a>' + '</li>');
+                  
+                   //  $(".article-container ul").append('<li>' + '<a href="' + element.short_url + '">' + '<img src="' + element.multimedia[4].url + '">' + '<p>' + element.title + '</p>' + '</a>' + '</li>');
+                  
+                   // $('.article-container ul').append('<li>' + element.multimedia[1].caption + '</li>');
+
+                  //  element.title // fetches title 
+                  
+                     $count++;
+                  }
                  
-                 
+
+
 
                 });
 

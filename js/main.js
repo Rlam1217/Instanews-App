bba39697338e6093cabecf3f75de0d460f6a4359
rlam1217 <rlam1217@gmail.com>
2019-08-15 13:18:10 -0700

    code clean up

--- js/main.js
+++ js/main.js
@@ -1,60 +1,33 @@
 $(function() {
 
   $('.article-container > .ajax-loader').hide();
-
     $('[name=categories]').on('change', function() {
       $('.article-container > .ajax-loader').show();
         const categories = $(this).val();
         
-        $('header').addClass('small-header')
+        $('header').addClass('small-header') //adds small header class
         
-
         $.ajax({
           method: "GET",
           url: 'https://api.nytimes.com/svc/topstories/v2/' + categories + '.json?api-key=nvza6g9rHUPtPoixuDjnr8OHmY9Br2p0'
         })
         
-        
         .done(function(data) {
            let $count = 0;
-            
-    
-           
              const results = $(data.results)
-             
-
              $('.article-container ul').empty(); //empties articles on category change
              $(results).each(function(index, element) {
-              
-                 
                   if (element.multimedia.length >= 5 && $count < 12) {
-                   
-                  
-
                   let image = '<div class="article-image" style="background-image:url(' + element.multimedia[4].url + ');">' + '</div>';
                   let url = '<a href="' + element.url + '">';
                   let p = '<p>' + element.abstract + '</p>';
-                  
-      
-                  
-
                    $('.article-container ul').append('<li class="article-list" >' + url + image + p + '</a>' + '</li>');
-                  
-                  
-                  
                      $count++;
                   }
                 })
                 }).always(function(data) {
-                
                   $('.article-container > .ajax-loader').hide();
-
-          
         });
-
-       
-
-
 
         });
 
